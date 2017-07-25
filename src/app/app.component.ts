ee133871ef4b995f53c3784254b4c8811c6cbc82
alan <alan@dub-001920-VM>
2017-07-25 15:36:07 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('animationState', [
+            state('false', style({
+                backgroundColor: 'LightGreen',
+                transform: 'scale(1) translateX(0%)'
+            })),
+            state('true', style({
+                backgroundColor: 'PaleGreen',
+                transform: 'scale(1.1) translateX(10%)'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-out'))
+        ])
+    ]
 })
 export class AppComponent {
-  title = 'app';
+
+        animation: boolean = false;
+
+    toggleAnimation() {
+        this.animation = !this.animation;
+        console.log(this.animation);
+    }
+
 }
